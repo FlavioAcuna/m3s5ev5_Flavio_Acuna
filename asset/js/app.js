2f5d02843023711509900888141101c7659b51e7
FlavioAcuña <42254195+FlavioAcuna@users.noreply.github.com>
2024-08-05 09:43:55 -0400

    modified:   index.html

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,25 +1,28 @@
 // Constructor de objeto Empleado
-function Empleado(nombre, salario, departamento) {
-  this.nombre = nombre;
-  this.salario = salario;
-  this.departamento = departamento;
+class Empleado {
+  constructor(nombre, salario, departamento) {
+    this.nombre = nombre;
+    this.salario = salario;
+    this.departamento = departamento;
+    // Método para calcular el salario después de un aumento
+    this.aumentarSalario = function (salarioAdd) {
+      this.salario += salarioAdd;
+      console.log(`Salario actualizado: ${this.salario}`);
+    };
+    // Método para cambiar el departamento
+    this.cambiarDepartamento = function (nuevoDepartamento) {
+      this.departamento = nuevoDepartamento;
+      console.log(`Departamento actualizado: ${this.departamento}`);
+    };
 
-  // Método para calcular el salario después de un aumento
-  this.aumentarSalario = function (salarioAdd) {
-    this.salario += salarioAdd;
-    console.log(`Salario actualizado: ${this.salario}`);
-  };
-
-  // Método para cambiar el departamento
-  this.cambiarDepartamento = function (nuevoDepartamento) {
-    this.departamento = nuevoDepartamento;
-    console.log(`Departamento actualizado: ${this.departamento}`);
-  };
-
-  // Método para obtener detalles del empleado
-  this.obtenerDetalles = function () {
-    return `Nombre: ${this.nombre}, Salario: ${this.salario}, Departamento: ${this.departamento}`;
-  };
+    // Método para obtener detalles del empleado
+    this.obtenerDetalles = function () {
+      return `Nombre: ${this.nombre}, Salario: ${this.salario}, Departamento: ${this.departamento}`;
+    };
+    this.insertarDetalleTabla = function () {
+      return empleado;
+    };
+  }
 }
 
 // Crear un nuevo empleado
@@ -29,8 +32,11 @@ console.log("Detalles iniciales del empleado:");
 console.log(empleado.obtenerDetalles());
 
 function actualizarDetalles() {
-  const detalles = empleado.obtenerDetalles();
-  document.getElementById("resultado").innerText = detalles;
+  const nombre = empleado.insertarDetalleTabla().nombre;
+  const salario = empleado.insertarDetalleTabla().salario;
+  const departamento = empleado.insertarDetalleTabla().departamento;
+  const tabla = document.getElementById("bodyTabla");
+  tabla.innerHTML = `<tr><td>${nombre}</td><td>${salario}</td><td>${departamento}</td></tr>`;
 }
 
 // Inicializar la interfaz con los detalles del empleado
